b79f037565b1ad9c748119bcb4a0c62f3860edc1
shivashankartummala <shivashankar.tummala@gmail.com>
2017-07-16 19:12:34 -0700

    Update SecondarySortDriver.java

--- ncdcSecondarySort2/src/SecondarySortDriver.java
+++ ncdcSecondarySort2/src/SecondarySortDriver.java
@@ -1,3 +1,13 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
 public class SecondarySortDriver extends Configured implements Tool {
       public int run(String[] args) throws Exception {
          Configuration conf = getConf();
