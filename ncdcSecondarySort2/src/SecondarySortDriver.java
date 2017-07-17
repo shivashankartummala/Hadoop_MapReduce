39f49c7946ebae2920c83abd9394cd150c1eb3e2
shivashankartummala <shivashankar.tummala@gmail.com>
2017-07-16 19:10:39 -0700

    Create SecondarySortDriver.java

---
+++ ncdcSecondarySort2/src/SecondarySortDriver.java
@@ -0,0 +1,44 @@
+public class SecondarySortDriver extends Configured implements Tool {
+      public int run(String[] args) throws Exception {
+         Configuration conf = getConf();
+          Job job = new Job(conf);
+          job.setJarByClass(SecondarySortDriver.class);
+          job.setJobName("SecondarySortDriver");
+ 
+          Path inputPath = new Path(args[0]);
+          Path outputPath = new Path(args[1]);
+         FileInputFormat.setInputPaths(job, inputPath);
+         FileOutputFormat.setOutputPath(job, outputPath);
+
+         job.setOutputKeyClass(TemperaturePair.class);
+         job.setOutputValueClass(NullWritable.class);
+
+        job.setMapperClass(SecondarySortingTemperatureMapper.class);
+         job.setReducerClass(SecondarySortingTemperatureReducer.class);
+        job.setPartitionerClass(TemperaturePartitioner.class);
+         job.setGroupingComparatorClass(YearMonthGroupingComparator.class);
+
+        boolean status = job.waitForCompletion(true);
+        theLogger.info("run(): status="+status);
+        return status ? 0 : 1;
+     }
+
+    /**
+    * The main driver for the secondary sort MapReduce program.
+    * Invoke this method to submit the MapReduce job.
+    * @throws Exception when there are communication
+    * problems with the job tracker.
+     */
+    public static void main(String[] args) throws Exception {
+        // Make sure there are exactly 2 parameters
+       if (args.length != 2) {
+            throw new IllegalArgumentException("Usage: SecondarySortDriver" +
+                                              " <input-path> <output-path>");
+       }
+        //String inputPath = args[0];
+        //String outputPath = args[1];
+        int returnStatus = ToolRunner.run(new SecondarySortDriver(), args);
+        System.exit(returnStatus);
+    }
+
+}
